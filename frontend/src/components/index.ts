061fa7e1d814957a62e30da80e8df78ad3d6b6d5
nitinn13 <nitinjha080@gmail.com>
2025-07-31 15:23:58 +0530

    file del fix

--- frontend/src/components/index.ts
+++ frontend/src/components/index.ts
@@ -3,4 +3,3 @@ export { default as Dashboard } from "./Dashboard";
 export { default as Layout } from "./Layout";
 export { default as Repositories } from "./Repositories";
 export { default as Prlist } from "./Prlist";
-export { default as Prcards } from "./Prcards";
